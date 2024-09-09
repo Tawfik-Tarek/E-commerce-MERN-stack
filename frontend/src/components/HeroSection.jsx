c6c9dc66804ec3d21a5904e0ad2daa7b71c6d7ff
Tawfik-Tarek <tawfiktarek1975@gmail.com>
2024-09-09 22:50:12 +0300

    make links instead of texts

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,20 +1,33 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Slider from "./Slider";
+
+const categories = [
+  "Woman’s Fashion",
+  "Men’s Fashion",
+  "Electronics",
+  "Home & Lifestyle",
+  "Medicine",
+  "Sports & Outdoor",
+  "Baby’s & Toys",
+  "Groceries & Pets",
+  "Health & Beauty"
+];
 
 const HeroSection = () => {
   return (
     <section className="flex flex-col md:flex-row justify-between items-center relative gap-5 w-full">
-      <aside className="flex-none border-r md:border-black pr-4 md:pr-6 px-4  w-full md:w-1/4">
+      <aside className="flex-none border-r md:border-black pr-4 md:pr-6 px-4 w-full md:w-1/4">
         <ul className="flex md:flex-col justify-center items-center md:justify-start gap-[15px] md:gap-0 flex-wrap md:flex-nowrap md:items-start md:space-y-3 py-4 md:py-8">
-          <li>Woman’s Fashion</li>
-          <li>Men’s Fashion</li>
-          <li>Electronics</li>
-          <li>Home & Lifestyle</li>
-          <li>Medicine</li>
-          <li>Sports & Outdoor</li>
-          <li>Baby’s & Toys</li>
-          <li>Groceries & Pets</li>
-          <li>Health & Beauty</li>
+          {categories.map((category, index) => (
+            <li key={index}>
+              <Link to={`/notARealLink/${category.replace(/[\s&’]+/g, "-").toLowerCase()}`}
+                className="hover:opacity-70 transition-all duration-300 ease-in"
+              >
+                {category}
+              </Link>
+            </li>
+          ))}
         </ul>
       </aside>
 
